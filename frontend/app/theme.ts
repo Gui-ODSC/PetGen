b3f3588509ef02929fd503883fd633e3ae24a51f
Gui Dev <guilhermecabrera51@gmail.com>
2026-04-25 14:10:08 -0300

    chore: ajustada as versões de pacotes do front

---
+++ frontend/app/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#000000",
+		},
+	},
+	typography: {
+		fontFamily: "Roboto, Arial, sans-serif",
+		h1: {
+			fontSize: "2.5rem",
+			fontWeight: 600,
+		},
+	},
+});
+
+export default theme;
