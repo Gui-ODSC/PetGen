1dff2c9eebd36e01e3d2de4f7ab589ba7ee8d993
Guilherme Oliveira <guilhermecabrera51@gmail.com>
2026-06-06 17:01:03 -0300

    chore: ajustada a instancia do axios para funcionar no login

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,11 +1,42 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
 
-const axiosInstance = axios.create({
-	baseURL: "http://localhost:8080",
+function getCookie(name: string): string | undefined {
+	if (typeof document === "undefined") return undefined;
+
+	return document.cookie
+		.split("; ")
+		.find((row) => row.startsWith(`${name}=`))
+		?.split("=")[1];
+}
+
+const baseConfig = {
 	withCredentials: true,
+	xsrfCookieName: "XSRF-TOKEN",
+	xsrfHeaderName: "X-XSRF-TOKEN",
 	headers: {
 		"Content-Type": "application/json",
+		Accept: "application/json",
 	},
+};
+
+export const api = axios.create({
+	baseURL: "http://localhost:8080/api",
+	...baseConfig,
 });
 
-export default axiosInstance;
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+	if (typeof window === "undefined") return config;
+
+	const token = getCookie("XSRF-TOKEN");
+
+	if (token) {
+		config.headers.set("X-XSRF-TOKEN", decodeURIComponent(token));
+	}
+
+	return config;
+});
+
+export const sanctumApi = axios.create({
+	baseURL: "http://localhost:8080",
+	...baseConfig,
+});
