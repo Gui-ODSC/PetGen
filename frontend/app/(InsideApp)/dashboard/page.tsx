3ee9ad17786301418a4c1cb7e555f47176ea9fe7
Guilherme Oliveira <guilhermecabrera51@gmail.com>
2026-06-14 14:34:24 -0300

    chore: removido o margin top da page

--- frontend/app/(InsideApp)/dashboard/page.tsx
+++ frontend/app/(InsideApp)/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { Grid, Typography } from "@mui/material";
 
 export default function DashboardPage() {
 	return (
-		<Grid container display={"flex"} mt={3} justifyContent={"center"}>
+		<Grid container display={"flex"} justifyContent={"center"}>
 			<Grid display={"flex"} flexDirection={"column"} alignItems={"center"}>
 				<Typography variant="h4">Dashboard</Typography>
 				<Typography>Bem-vindo ao dashboard!</Typography>
